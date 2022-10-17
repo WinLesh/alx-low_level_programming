0da5ff1f64a4a76d9713948a0ea6828be050a19d
WinLesh <l.seshabo@gmail.com>
2022-10-17 08:27:42 -0700

    ALxis not for the lazy

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,4 +7,30 @@
  */
 int main(void)
 {
+	int p;
+	int q;
 
+	for (p = 0; p <= 99; p++)
+	{
+		for (q = 0; q <= 99; q++)
+		{
+			if (p < q)
+			{
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
+				putchar(' ');
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
