c618961b8479b54b0c396f1eb72ba482895b52e5
WinLesh <l.seshabo@gmail.com>
2022-10-19 13:39:08 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,11 +2,11 @@
 #define MAIN_H
 #include<stdio.h>
 
-int _putchar(char bc);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int a);
-int _isalpha(int a);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -14,6 +14,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int a);
+
 
 #endif /* MAIN_H */
