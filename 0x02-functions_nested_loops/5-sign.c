14fa3fc6861fb566dd18888bea6179872459abfa
WinLesh <l.seshabo@gmail.com>
2022-10-19 15:56:30 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_sign - print + if n is greater than zero
+ * 0 if n is zero and - if n is less than zero
+ * @n: takes integer type input for function.
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
