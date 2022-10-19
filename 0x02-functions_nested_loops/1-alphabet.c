7b0aeacd86a05d20b84d805da27ca954a15fd9bc
WinLesh <l.seshabo@gmail.com>
2022-10-19 14:20:37 -0700

    main.c gives me errors :(

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  *print_alphabet - utilizes on the _putchar function to print
  *the alphabet a - z
  */
+
 void print_alphabet(void)
 {
-	char l;
+	char lc;
 
-	for (l = 'a'; l <= 'z'; l++)
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
-		_putchar(l);
+		putchar(lc);
 	}
-	_putchar('\n');
+	putchar('\n');
