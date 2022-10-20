eeca376f050a63d729c49a89b0c9b895c23a595e
WinLesh <l.seshabo@gmail.com>
2022-10-20 00:29:08 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,13 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n, ", n);
-	}
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			printf("%d, ", count);
 	else
-	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n, ", n);
-	}
+		for (count = n; count < 98; count++)
+			printf("%d, ", count);
+	printf("98\n");
 }
