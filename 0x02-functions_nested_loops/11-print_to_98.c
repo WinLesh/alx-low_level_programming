ec61abc31bd7a57a587fb11d3362b66240814bdb
WinLesh <l.seshabo@gmail.com>
2022-10-19 17:05:19 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_to_98 - print n to 98 counts
+ * @n: print from this number
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n, ", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n, ", n);
+	}
+}
