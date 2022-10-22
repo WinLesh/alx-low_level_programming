197bbec6f1e0fe4b9664bedb800c15b8ee74ac00
WinLesh <l.seshabo@gmail.com>
2022-10-21 17:19:00 -0700

    i want togiveup

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - check if c is upper
+ * @c: input for alphabet
+ *  Return: 1 if its uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
