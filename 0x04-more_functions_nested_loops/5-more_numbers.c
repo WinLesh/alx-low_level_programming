bf4af01465a36021a99f3e9a90003259166dfbc7
WinLesh <l.seshabo@gmail.com>
2022-10-21 18:24:34 -0700

    i will be back

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,12 @@
+#include"main.c"
+/**
+ *more_numbers - prints 10 times the numbers, from 0 to 14
+ *followed by new line
+ *can only use _putchar three times
+ *Return: Always 0
+ */
+void more_numbers(void)
+{
+	int l;
+
+
