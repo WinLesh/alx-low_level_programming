f7ce4e6035c587d82ab6cb9b01d84e82e784579c
WinLesh <l.seshabo@gmail.com>
2022-10-21 17:40:46 -0700

    i am rising

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ *print_numbers - print 0 - 9
+ *only using _putchar twice
+ *Return: void
+ *
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	for (num = 10; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
+
