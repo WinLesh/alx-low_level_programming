304ccb4acbfdd5ec0311507466d6c89ce7af8781
WinLesh <l.seshabo@gmail.com>
2022-10-28 00:55:24 -0700

    hard things

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _strncpy - A function that copies a string.
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ * Return: @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
