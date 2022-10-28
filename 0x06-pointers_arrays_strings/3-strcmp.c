1bafca677a1d38e156f7d4a8e7644ff4f4517e92
WinLesh <l.seshabo@gmail.com>
2022-10-28 01:06:44 -0700

    Doing hard things

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: string 1 input to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 if s1 and s2 are equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, r;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
+	}
+	return (r);
+}
