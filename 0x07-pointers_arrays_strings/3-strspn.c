598df18ce46b7ae5b865c0ede3f4aea000f9e6d7
WinLesh <l.seshabo@gmail.com>
2022-11-08 06:47:19 -0800

    I will keep trying

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *_strspn - Gets the length of a prefix substring.
+ *@s: The string to be searched.
+ *@accept: The prefix to be measured.
+ *Return: The number of bytes in s which
+ *consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = 0, b;
+	int targetmatches = 0;
+
+	while (s[a] != '\0') /*iterate through string*/
+	{
+		for (b = 0; accept[b] != '\0'; b++) /*iterate through target*/
+		{
+			if (s[a] == accept[b]) /*record & break at first match*/
+			{
+				targetmatches++;
+				break;
+			}
+			if (accept[b + 1] == '\0' && s[a] != accept[b])
+
+				return (targetmatches);/*return if idx doesn't match*/
+		}
+		a++;
+	}
+	return (targetmatches); /* return num if all match till end */
+}
