dd9af7024d232f7db4f93bbdb2401a69eefd7365
WinLesh <l.seshabo@gmail.com>
2022-11-09 01:45:11 -0800

    No Julien Chess is a refresher

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include <stdio.h>
 /**
  *
  * print_chessboard - Prints a chessboard.
@@ -9,7 +10,6 @@ void print_chessboard(char (*a)[8])
 	int i, j;
 
 	for (i = 0; i < 8; i++)
-
 	{
 		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
