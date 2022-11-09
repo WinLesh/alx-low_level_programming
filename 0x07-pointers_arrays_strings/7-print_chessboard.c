51000169a0c753eaa22863ba5edad09e6afa09cf
WinLesh <l.seshabo@gmail.com>
2022-11-09 03:53:59 -0800

    Jlien

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,7 +6,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int indx1, indx2
-	for (indx1 = 0;eiei a[indx1][7]; indx1++)
+	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
 		for (indx2 = 0; indx2 < 8; indx2++)
 			_putchar(a[indx1][indx2]);
