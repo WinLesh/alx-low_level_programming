4bade08fcbd2bcac8c74eb2144365e32a21b13f8
WinLesh <l.seshabo@gmail.com>
2022-11-09 11:05:36 -0800

    Julien chess is not bad

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,12 +6,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int c, b;
+	unsigned int c, b = 0;
 
 	for (c = 0; c < 8; c++)
 	{
-		for (b = 0; b < 8; b++)
-			_putchar(a[c][b]);
-		_putchar('\n');
+		if (c % 8 == 0 && c != 0)
+		{
+			b = c;
+			_putchar('\n');
+		}
+			_putchar(a[c / 8][c - b]);
 	}
+	_putchar('\n');
 }
