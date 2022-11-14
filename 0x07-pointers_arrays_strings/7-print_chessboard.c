bc71ab14c36d3d536d6fabcfbc945fe539a750f6
WinLesh <l.seshabo@gmail.com>
2022-11-14 15:13:06 -0800

    Julien chess is fun

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- *print_chessboard - Prints a chessboard.
- *@a: The chessboard to be printed.
+ *print_chessboard - prints the chessboard
+ *@a: input pointer.
  *Return: no return.
  */
 void print_chessboard(char (*a)[8])
 {
-	int row;
-	int column;
+	unsigned int i, m = 0;
 
-	for (row = 0; row < 8; row++)
+	for (i = 0; i < 64; i++)
 	{
-		for (column = 0; column < 8; column++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[row][column]);
+			m = i;
+			_putchar('\n');
 		}
-		_putchar('\n');
+		_putchar(a[i / 8][i - m]);
 	}
+	_putchar('\n');
 }
