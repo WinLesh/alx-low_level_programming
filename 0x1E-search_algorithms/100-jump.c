8820346b85a443869844571b8b3be930fb15c243
WinLesh <l.seshabo@gmail.com>
2023-05-11 23:54:14 -0700

    LEsego

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,44 +1,79 @@
 #include "search_algos.h"
 #include <math.h>
-
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
+ *  * linear_search_mod - using linear search in order to get
+ *   * the speficied value
+ *    * @begin: first position to evaluate
+ *     * @last: last position to evaluate
+ *      * @value: value to search inside the array
+ *       * @array: array to search
+ *        * Return: value or -1 if not in the array
+ *         */
+int linear_search_mod(int begin, int last, int value, int *array)
+{
+		int c = begin;
+
+			for (; c <= last; c++)
+					{
+								if (value == array[c])
+											{
+															break;
+																	}
+										else
+													{
+																	printf("Value checked array[%i] = [%i]\n", c, array[c]);
+																			}
+											}
+				if (c > last)
+						{
+									return (-1);
+										}
+					printf("Value checked array[%i] = [%i]\n", c, array[c]);
+						return (c);
+}
+/**
+ *  * jump_search - use jump search algorithm in order to get
+ *   * the value specified, here you have to you sqrt function
+ *    * to get the pace
+ *     * @array: array to search
+ *      * @size: size of array
+ *       * @value: value to search
+ *        * Return: index or -1 if it is not inside the array
+ *         */
 int jump_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
+		int prev = 0, actual, paso;
 
-	if (array == NULL || size == 0)
-		return (-1);
+			if (array == NULL)
+						return (-1);
 
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
+				paso = sqrt((int)size);
 
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+					for (actual = paso; actual < (int)size; actual += paso)
+							{
+										prev = actual - paso;
+												if (array[prev] > value && prev == 0)
+															{
+																			printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
+																						return (-1);
+																								}
+														else if (array[actual] >= value)
+																	{
+																					printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
+																								break;
+																										}
+																else
+																				printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
+																	}
 
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
+						if (actual >= (int)size)
+								{
+											prev = actual - paso;
+													printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
+															printf("Value found between indexes [%i] and [%i]\n", prev, actual);
+																	return (linear_search_mod(prev, size - 1, value, array));
+																		}
 
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-
-	for (; prev <= index && prev < (int)size; prev++)
-	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
-	}
-
-	return (-1);
+							printf("Value found between indexes [%i] and [%i]\n", prev, actual);
+								return (linear_search_mod(prev, actual, value, array));
 }
